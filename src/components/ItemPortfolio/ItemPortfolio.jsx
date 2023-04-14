b0efbdf2613caf311488b0e065e654400af9fe82
Pelezi <abcsandro@hotmail.com>
2023-04-14 11:50:59 -0300

    revert

--- src/components/ItemPortfolio/ItemPortfolio.jsx
+++ src/components/ItemPortfolio/ItemPortfolio.jsx
@@ -8,7 +8,7 @@ function ItemPortfolio(props) {
     return(
         <div className="item-portfolio">
             <a href={props.link} target="_blank" >
-                <img src={"/src/assets/" + props.image} alt="A Giant TARDIS built in minecraft" />
+                <img src={TARDIS} alt="A Giant TARDIS built in minecraft" />
                 <p>{props.title}</p>
                 <small>{props.description}</small>
             </a>
